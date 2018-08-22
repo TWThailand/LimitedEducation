1e365b36e2ef9aa24ffb618c49d19115ed3a919b
Vikhyath <vikhyatc@thoughtworks.com>
2018-08-23 01:14:42 +0700

    [#44] [Viky] #Home page base layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,79 @@
 <template>
     <div class="container">
+      <div class="logo"/>
+      <div class="desc">This is a place holder for the description of the website</div>
+      <div class="hashtag">#LimitedEducation</div>
+      <section class="items">
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+        <div class="item">
+          <span class="count">Only 3000 left</span>
+          <div class="productImage"/>
+          <div class="productDesc">
+            <span class="title">New Title</span>
+            <span class="brand-name">Some Brand</span>
+            <span class="amount">only 2999 Bhat</span>
+          </div>
+          <button class="perorder">per-order</button>
+        </div>
+      </section>
     </div>
 </template>
