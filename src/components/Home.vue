81e93f76f17c0f92499b9347589e3f9c974b3929
Vikhyath <vikhyatc@thoughtworks.com>
2018-08-23 09:00:09 +0700

    [#44] [Viky] #Addded vote section in home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,78 +3,140 @@
       <div class="logo"/>
       <div class="desc">This is a place holder for the description of the website</div>
       <div class="hashtag">#LimitedEducation</div>
-      <div class="category-heading">Per-order</div>
-      <section class="items">
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+      <div class="category">        
+        <div class="category-heading">Per-order</div>      
+        <section class="items">
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
-        </div>
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
-        </div>
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
-        </div>
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
-        </div>
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
-        </div>
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
-        </div>
-        <div class="item">
-          <span class="count">Only 3000 left</span>
-          <div class="productImage"/>
-          <div class="productDesc">
-            <span class="title">New Title</span>
-            <span class="brand-name">Some Brand</span>
-            <span class="amount">only 2999 Bhat</span>
+          <div class="item">
+            <span class="count">Only 3000 left</span>
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">only 2999 Bhat</span>
+            </div>
+            <button class="perorder">Per-order</button>
           </div>
-          <button class="perorder">Per-order</button>
+        </section>
+      </div>
+      <div class="category">        
+        <div class="category-heading">Vote for you favorite brand</div>      
+        <section class="items">
+          <div class="item">
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">Total votes 999</span>
+            </div>
+            <button class="vote">Vote</button>
+          </div>
+          <div class="item">
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">Total votes 299</span>
+            </div>
+            <button class="vote">Vote</button>
+          </div>
+          <div class="item">
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">Total votes 499</span>
+            </div>
+            <button class="vote">Vote</button>
+          </div>
+          <div class="item">
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">Total votes 209</span>
+            </div>
+            <button class="vote">Vote</button>
+          </div>
+          <div class="item">
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">Total votes 5999</span>
+            </div>
+            <button class="vote">Vote</button>
+          </div>
+          <div class="item">
+            <div class="productImage"/>
+            <div class="productDesc">
+              <span class="title">New Title</span>
+              <span class="brand-name">Some Brand</span>
+              <span class="amount">Total votes 2999</span>
+            </div>
+            <button class="vote">Vote</button>
+          </div>
+        </section>
         </div>
-      </section>
+      </div>
     </div>
 </template>
