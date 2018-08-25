e3861f5b5d4615db04622af6bdc23faf0e71490a
Sriram <sriramsb@thoughtworks.com>
2018-08-25 16:12:49 +0700

    [Sriram] Setting the coverage threshold

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -27,5 +27,13 @@ module.exports = {
     '!src/main.js',
     '!src/router/index.js',
     '!**/node_modules/**'
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 }
