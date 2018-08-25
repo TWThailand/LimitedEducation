a1c61b397e9920805b8a27a230f8ee2522e9d4df
Sriram <sriramsb@thoughtworks.com>
2018-08-25 17:00:46 +0700

    [Sriram] Added tests around the quantity of the items displayed

--- test/unit/specs/Item.spec.js
+++ test/unit/specs/Item.spec.js
@@ -1,30 +1,23 @@
 import Vue from 'vue'
 import Item from '@/components/Item'
 
-describe("testing the item component", () => {
+describe('testing the item component', () => {
     let Constructor, vm, propsData
-    describe("Pre Order Items", () => {
+    describe('Pre Order Items', () => {
         beforeEach(() => {
             Constructor = Vue.extend(Item)
             propsData = {
                 productType: "PreOrder"
             }
         })
-        describe("Price of the item", ()=> {
+        describe('Price of the item', ()=> {
             it('should render the price correctly', () => {
                 propsData.price = 150
                 vm = new Constructor({
                     propsData
                 }).$mount()
                 expect(vm.$el.querySelector('.amount').textContent)
-                  .toEqual('only 150 Baht')
-            })
-            it('When price is undefined', () => {
-                vm = new Constructor({
-                    propsData
-                }).$mount()
-                expect(vm.$el.querySelector('.amount').textContent)
-                  .toEqual('only undefined Baht')
+                    .toEqual('only 150 Baht')
             })
             it('When price is in decimals', () => {
                 propsData.price=150.45
@@ -32,7 +25,31 @@ describe("testing the item component", () => {
                     propsData
                 }).$mount()
                 expect(vm.$el.querySelector('.amount').textContent)
-                  .toEqual('only 150.45 Baht')
+                    .toEqual('only 150.45 Baht')
+            })
+            it('When price is undefined', () => {
+                vm = new Constructor({
+                    propsData
+                }).$mount()
+                expect(vm.$el.querySelector('.amount').textContent)
+                    .toEqual('only undefined Baht')
+            })
+        })
+        describe('Quantity of item left to purchase', () => {
+            it('should render the quantity correctly', () => {
+                propsData.quantity = 1000
+                vm = new Constructor({
+                    propsData
+                }).$mount()
+                expect(vm.$el.querySelector('.count').textContent)
+                    .toEqual('Only 1000 left')
+            })
+            it('When quantity is undefined', () => {
+                vm = new Constructor({
+                    propsData
+                }).$mount()
+                expect(vm.$el.querySelector('.count').textContent)
+                    .toEqual('Only  left')
             })
         })
     })
