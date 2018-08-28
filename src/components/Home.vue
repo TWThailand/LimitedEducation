d17614e0f24bb6b9c723163e93779b204c787762
“teerasak” <teerasak@thoughtworks.com>
2018-08-28 09:45:37 +0700

    [Tua] refactor to use list rendering

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,56 +6,23 @@
     <div class="category">
       <div class="category-heading">Pre-order</div>
       <section class="items">
-        <item
-          productType="PreOrder"
-          :quantity=1000
-          name="Limited Shoe"
-          brand="TW shoes"
-          :price=299
-        />
-        <item
-          productType="PreOrder"
-          :quantity=2000
-          name="Limited Pizza"
-          brand="TW Pizza"
-          :price=599
-        />
-        <item
-          productType="PreOrder"
-          :quantity=3000
-          name="Limited Steak"
-          brand="TW Steak house"
-          :price=785000
-        />
-        <item
-          productType="PreOrder"
-          :quantity=4100
-          name="Limited Phone"
-          brand="TW mobile"
-          :price=1200
+        <item v-for="item in items" v-if="item.productType == 'PreOrder'"
+          v-bind:productType="item.productType"
+          v-bind:quantity="item.quantity"
+          v-bind:name="item.name"
+          v-bind:brand="item.brand"
+          v-bind:price="item.price"
         />
       </section>
     </div>
     <div class="category">
       <div class="category-heading">Vote for you favorite brand</div>
       <section class="items">
-        <item
-          productType="Vote"
-          name="Black drink"
-          brand="TW cola"
-          :totalVote=200
-        />
-        <item
-          productType="Vote"
-          name="Black Steak"
-          brand="TW Steak house"
-          :totalVote=50
-        />
-        <item
-          productType="Vote"
-          name="ที่เป่าลม"
-          brand="TW gadget"
-          :totalVote=120
+        <item v-for="item in items" v-if="item.productType == 'Vote'"
+          v-bind:productType="item.productType"
+          v-bind:name="item.name"
+          v-bind:brand="item.brand"
+          v-bind:totalVote="item.totalVote"
         />
       </section>
     </div>
@@ -65,6 +32,19 @@
   import Item from "./Item";
 
   export default {
-    components: {Item}
+    components: {Item},
+    data: function() {
+      return {
+        items: [
+          {productType: 'PreOrder', quantity: 4100, name: 'Limited Phone', brand: 'TW mobile', price: 1200},
+          {productType: 'PreOrder', quantity: 3000, name: 'Limited Steak', brand: 'TW Steak House', price: 785000},
+          {productType: 'PreOrder', quantity: 2000, name: 'Limited Pizza', brand: 'TW Pizza', price: 599},
+          {productType: 'PreOrder', quantity: 1000, name: 'Limited Shoe', brand: 'TW Shoes', price: 299},
+          {productType: 'Vote', name: 'Black drink', brand: 'TW Cola', totalVote: 200},
+          {productType: 'Vote', name: 'Black Steak', brand: 'TW Steak House', totalVote: 50},
+          {productType: 'Vote', name: 'ที่เป่าลม', brand: 'TW Gadget', totalVote: 120},
+        ]
+      }
+    }
   }
 </script>
