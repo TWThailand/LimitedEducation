85c006c70b5414d713406cc10317a2e611f89b86
Martina Duemcke <mduemcke@thoughtworks.com>
2018-08-29 16:56:21 +0700

    [Martina] Clear input fields on focus

--- src/components/vote/VoteViaFacebookForm.vue
+++ src/components/vote/VoteViaFacebookForm.vue
@@ -4,8 +4,8 @@
     <div class="wrapper">
       <div class="title">ร่วมลงชื่อสนับสนุน</div>
       <div class="subtitle">สินค้าที่มีผลโหวตสูงสุดของแต่ละแบรนด์จะนำไปผลิตจริง</div>
-      <input type="text" value="ชื่อจริง นามสกุล (ภาษาไทย)" class="first-name"/>
-      <input type="text" value="ชื่อเล่น (ภาษาไทย)" class="last-name"/>
+      <input type="text" placeholder="ชื่อจริง นามสกุล (ภาษาไทย)" class="first-name"/>
+      <input type="text" placeholder="ชื่อเล่น (ภาษาไทย)" class="last-name"/>
       <div class="product-text">สนับสนุน Club 21 ออก Limited edition เพื่อนำรายได้ทั้งหมด ไปช่วยการศึกษาไทย (ผลิตภัณฑ์ที่ผลิตจะเป็นแบบตามที่อยู่ในรูป)</div>
       <input type="submit" value="สนับสนุนสินค้าเพื่อช่วยการศึกษา"/>
     </div>
