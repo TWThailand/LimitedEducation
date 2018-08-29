b316068901d4adb2323884ef0b0ee13de91e982f
Martina Duemcke <mduemcke@thoughtworks.com>
2018-08-29 10:32:42 +0700

    styling the page using flexbox

--- src/components/vote/detail.vue
+++ src/components/vote/detail.vue
@@ -1,19 +1,21 @@
 <template>
-    <div id="vote-detail">
-      <div id="logo-section" class="row">
-        <button class="col-xs-1"><</button>
-        <div class="col-xs-4">
-          <img src="../../assets/logo.png" class="logo-limited-education"/>
-        </div>
-        <div class="vertically-aligned-x col-xs-2">x</div>
-        <div class="col-xs-4">
-          <img src="../../assets/logo-nanyang.png" class="logo-product" />
-        </div>
-        <button class="col-xs-1">></button>
-      </div>
-      <div id="product-image">
-      </div>
-        <div id="description">
+  <div id="vote-detail">
+    <div id="logo-section" class="flex-parent">
+      <button class="previous-product flex-child"><</button>
+      <img src="../../assets/logo.png" class="logo-limited-education flex-child"/>
+      <div class="vertically-aligned-x flex-child">x</div>
+      <img src="../../assets/logo-nanyang.png" class="logo-product flex-child" />
+      <button class="next-product flex-child">></button>
+    </div>
+    <div id="product-image" class="flex-parent">
+      <img src="../../assets/product-shoe-nanyang.jpg" class="product-image-left flex-child"/>
+      <img src="../../assets/logo.png" class="product-image-right flex-child"/>
+    </div>
+    <div id="product-description">
+      <div class="title">The oportunity shoe</div>
+      <div class="content-text">
+        Super cool nanyang shoe with limited education logo and own name in Thai! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </div>
+  </div>
 </template>
